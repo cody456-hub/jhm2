6b495f7198ebde2650c361e51b04e45a62a9bc8f
cody456-hub <abcxjnc@gmail.com>
2025-02-08 09:45:34 +0800

    update

--- practise/weather.py
+++ practise/weather.py
@@ -1,7 +1,6 @@
 import requests
 import matplotlib.pyplot as plt
 
-# 定义 OpenWeatherMap API 的基本信息
 api_key = '65e48f988ae30c4af07ee29855ec28f7'
 base_url = 'https://api.openweathermap.org/data/2.5/weather'
 
@@ -17,25 +16,25 @@ def get_weather_data(city_name):
     response = requests.get(base_url, params=params)
     return response.json()
 
+
 def plot_weather_data(weather_data):
     fig, ax = plt.subplots()
     ax.set_title('Weather parameters Visualization')
     
-    # 提取天气数据
     temperature = weather_data['main']['temp']
     humidity = weather_data['main']['humidity']
     wind_speed = weather_data['wind']['speed']
     weather_condition = weather_data['weather'][0]['description']
     uv_index = weather_data['uv_index']
 
-    # 打印天气数据
+
     print(f"Temperature: {temperature}°C")
     print(f"Humidity: {humidity}%")
     print(f"Wind Speed: {wind_speed} m/s")
     print(f"Weather Condition: {weather_condition}")
     print(f"UV Index: {uv_index}")
     
-    # 绘制图表
+    
     ax.plot([temperature], label='Temperature (°C)')
     ax.plot([humidity], label='Humidity (%)')
     ax.plot([wind_speed], label='Wind Speed (m/s)')
