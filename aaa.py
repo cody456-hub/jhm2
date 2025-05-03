04b4b51adfc830ead6164c17480cd1b6c603d08e
cody456-hub <abcxjnc@gmail.com>
2025-05-03 10:27:28 +0000

    update

--- aaa.py
+++ aaa.py
@@ -1,21 +1,4 @@
-import requests
-from bs4 import BeautifulSoup
 import pandas as pd
 
-# Barchart 的 APY 歷史數據頁面
-url = "https://www.barchart.com/stocks/quotes/APY/historical-data"
-
-# 發送請求
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-
-# 找到數據表（需要查看網頁結構以確保準確抓取）
-table = soup.find("table")
-
-# 如果找到表格，就轉換成 CSV（這部分需要根據網頁的 HTML 調整）
-if table:
-    df = pd.read_html(str(table))[0]
-    df.to_csv("APY_stock_data.csv", index=False)
-    print("APY 的數據已保存到 APY_stock_data.csv")
-else:
-    print("未找到 APY 的股票數據，請確認網站結構！")
+df = pd.read_csv("APY_stock_data.csv")
+print(df.head())
